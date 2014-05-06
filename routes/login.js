77033119bba7a3656a4f415f86901c5f1098722b
OIT PC <coke770911@gmail.com>
2014-05-06 18:22:33 +0800

    測試
    OIT PC

--- routes/login.js
+++ routes/login.js
@@ -6,11 +6,27 @@ var md5 = require('MD5');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-	res.send('a');
+	res.render('login', { title: 'login page' });
 });
 
-router.ChkAccount = function(req, res){
-	res.send(md5('1713'));
+
+
+router.ChkAccount = function(req, res) {
+	var account = req.param('account');
+	var password = md5(req.param('password'));
+	var sql = "SELECT * FROM tb_member WHERE account = ? AND password = ?";
+	
+
+	conn.query(sql,[account,password],function(err, rows, fields) {
+		if(err) {
+			res.send("資料庫連線錯誤");
+		} 
+
+		
+		console.dir(rows);
+		console.dir(rows.length);
+		
+	});
 }
 
 module.exports = router;
