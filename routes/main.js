14007e35bb0fc37d7246e63f4ae70335e27115d5
Joey Mac <coke770911@gmail.com>
2014-05-15 19:27:48 +0800

    終於可以session了
    yaya 成功

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	console.dir(res.session);
+	console.dir(req.session);
 	res.render('main', {title:'首頁'});
 });
 
